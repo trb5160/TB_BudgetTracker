be2afd135ddb9aed5bb3c3927d2ac272347cd407
Tom Barber <trb5160@gmail.com>
2021-07-11 19:19:55 -0400

    removed assets folder, got transactions working

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,13 +2,13 @@ var CACHE_NAME = "my-site-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 var urlsToCache = [
-  "/",
-  "/assets/java/db.js",
-  "/assets/java/index.js",
-  "/assets/manifest.json",
-  "/assets/css/styles.css",
-  "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png"
+    "/",
+    "/db.js",
+    "/index.js",
+    "/manifest.json",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
 ];
 
 self.addEventListener("install", function(event) {
