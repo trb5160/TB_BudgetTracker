808d93d2b9c10a2686bfbc5c62d62fab0e2b0940
Tom Barber <trb5160@gmail.com>
2021-07-12 11:09:59 -0400

    updated code to try and get heroku deployment live

--- server.js
+++ server.js
@@ -16,12 +16,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-    useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/HerokuApps',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
