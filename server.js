f08a957da553f33fac440d332f2d33bfbee5f301
Tom Barber <trb5160@gmail.com>
2021-07-21 20:19:48 -0400

    updated code for heroku deploy

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/HerokuApps',
+  process.env.MONGODB_URI || 'mongodb://localhost/Budget',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
