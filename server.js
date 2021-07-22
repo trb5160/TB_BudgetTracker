b9363ce9f88cb96f70eeaa60a587ed87120aec2f
Tom Barber <trb5160@gmail.com>
2021-07-21 20:43:46 -0400

    hoping to get heroku working

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 const apiRoutes = require("./routes/api.js");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -16,18 +16,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/Budget',
-  {
-    useNewUrlParser: true,
+const mongoUri = process.env.MONGODB_URI || "mongodb://localhost/Budget";
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false
-  }
-);
+});
 
-// routes
-app.use(require("./routes/api.js"));
+app.use(apiRoutes);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
